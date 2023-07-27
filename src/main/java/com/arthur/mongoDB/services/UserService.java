46cfbee5feb4dc91740f3eee103930f6d422d1a3
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-27 16:05:46 -0300

    Obtendo um usuário por id

--- src/main/java/com/arthur/mongoDB/services/UserService.java
+++ src/main/java/com/arthur/mongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.arthur.mongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arthur.mongoDB.domain.User;
 import com.arthur.mongoDB.repository.UserRepository;
+import com.arthur.mongoDB.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
