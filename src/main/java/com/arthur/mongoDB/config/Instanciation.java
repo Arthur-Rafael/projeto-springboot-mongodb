b29bebdda3572029324a666e1fc07a1751722e40
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-28 10:27:28 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/arthur/mongoDB/config/Instanciation.java
+++ src/main/java/com/arthur/mongoDB/config/Instanciation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.arthur.mongoDB.domain.Post;
 import com.arthur.mongoDB.domain.User;
+import com.arthur.mongoDB.dto.AuthorDTO;
 import com.arthur.mongoDB.repository.PostRepository;
 import com.arthur.mongoDB.repository.UserRepository;
 
@@ -35,8 +36,10 @@ public class Instanciation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra são paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra são paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
